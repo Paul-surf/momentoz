ecaa7bb179cc28ede662ad13afd3d48eecc7769e
Paul <72189188+Paul-surf@users.noreply.github.com>
2023-11-21 14:19:24 +0100

    removed main method

--- WebAPI/CustomerRestService/Program.cs
+++ WebAPI/CustomerRestService/Program.cs
@@ -28,31 +28,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-
-
-
-
-
-{
-    static void Main()
-    {
-        // Tilføj certifikatvalidering
-       ServicePointManager.ServerCertificateValidationCallback = ValidateCertificate;
-
-        // Udfør HTTPS-anmodning til serveren
-        using (var client = new WebClient())
-        {
-            var result = client.DownloadString("https://localhost");
-            Console.WriteLine(result);
-        }
-    }
-
- //   Certifikatvalideringsmetode
-    static bool ValidateCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-    {
-        // Returner "sandt" for at acceptere certifikatet
-        return true;
-    }
-}
