116c5e3b96885e0e5915fabd712e2b65155205b5
DatSUPER <98950270+DatSUPER@users.noreply.github.com>
2023-11-23 12:21:26 +0100

    tilføjet til models

---
+++ WebAPI/CustomerData/ModelLayer/CheckedBagage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseData.ModelLayer
+{
+    public class CheckedBagage
+    {
+        public int Weight { get; set; }
+
+        public CheckedBagage(int weight = 20)
+        {
+            Weight = weight;
+        }
+    }
+}
--- WebAPI/CustomerData/ModelLayer/Ticket.cs
+++ WebAPI/CustomerData/ModelLayer/Ticket.cs
@@ -2,36 +2,19 @@ namespace DatabaseData.ModelLayer
 {
     public class Ticket
     {
-        public Ticket(string? firstName)
-        {
-        }
+       public string TicketType { get; set; }
+       public int TicketNumber { get; set; }
 
-        public Ticket(string? firstName, string? lastName, string? mobilePhone, string? email)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            MobilePhone = mobilePhone;
-            Email = email;
-        }
+        public Bagage Bagage { get; set; }
 
-        public Ticket(int id, string? firstName, string? lastName, string? mobilePhone, string? email)
-            : this(firstName, lastName, mobilePhone, email)
-        {
-            Id = id;
-        }
+        public Flight Flight { get; set; }
 
-        public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? MobilePhone { get; set; }
-        public string? Email { get; set; }
 
-        public bool IsTicketEmpty
-        {
-            get
-            {
-                return String.IsNullOrWhiteSpace(FirstName) || String.IsNullOrWhiteSpace(LastName);
-            }
-        }
+
+
+
+
+
+
     }
 }
