c5e58af1814d009459e6856efc9a8b4875e0c4a9
tiseksogfyrre <10461241@ucn.dk>
2023-11-27 12:56:45 +0100

    Desapp Flght ikke færdg mange fejl

---
+++ Client/MomentozClientApp/Servicelayer/FlightServiceAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomentozClientApp.Servicelayer
+{
+    public class FlightServiceAccess : IFlightConnection
+    {
+        private readonly IGeometryDataAccess _flightServiceAccess;
+
+        public FlighServiceAccess()
+        {
+            _flightServiceAccess = new FlightServiceAccess();
+        }
+
+        public async Task<List<Flight>?> GetAllFlights()
+
+        {
+            List<Flight>? foundFlights;
+            try
+            {
+                foundFlights = await _flightServiceAccess.GetLines();
+            }
+            catch
+            {
+                foundFlights = null;
+            }
+
+            return foundFlights;
+        }
+    }
+}
