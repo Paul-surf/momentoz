77ba1a8f99d706e2a75ac7d7d35230771a84fb15
tiseksogfyrre <10461241@ucn.dk>
2023-11-28 19:21:48 +0100

    ticket og baggage til desk

---
+++ Client/MomentozClientApp/BusinessLogicLayer/BaggageDataControl.cs
@@ -0,0 +1,77 @@
+﻿using MomentozClientApp.DTOs;
+using MomentozClientApp.ModelLayer;
+using MomentozClientApp.Servicelayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomentozClientApp.BusinessLogicLayer
+{
+    public class BaggageDataControl : IBaggagedata
+    {
+        private readonly IBaggageAccess _baggageAccess;
+
+        public BaggageDataControl(IBaggageAccess inBaggageAccess)
+        {
+            _baggageAccess = inBaggageAccess;
+        }
+
+        public BaggageDto? Get(int id)
+        {
+            try
+            {
+                // Synchronously waiting on an asynchronous method.
+                var foundBaggage = _baggageAccess.GetBaggageById(id).Result;
+                return ModelConversion.BaggageDtoConvert.FromBaggage(foundBaggage);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public List<BaggageDto>? Get()
+        {
+            try
+            {
+                // Synchronously waiting on an asynchronous method.
+                var foundBaggages = _baggageAccess.GetBaggageAll().Result;
+                return ModelConversion.BaggageDtoConvert.FromBaggageCollection(foundBaggages);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public int Add(BaggageDto baggageToAdd)
+        {
+            try
+            {
+                var newBaggage = ModelConversion.BaggageDtoConvert.ToBaggage(baggageToAdd);
+                if (newBaggage != null)
+                {
+                    // Synchronously waiting on an asynchronous method.
+                    return _baggageAccess.CreateBaggage(newBaggage).Result;
+                }
+                return 0;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public bool Put(BaggageDto baggageToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Client/MomentozClientApp/Servicelayer/ITicketAccess.cs
@@ -0,0 +1,14 @@
+﻿using MomentozClientApp.Model;
+using MomentozClientApp.ModelLayer;
+
+namespace MomentozClientApp.Servicelayer
+{
+    internal interface ITicketAccess
+    {
+        Task<Ticket> GetTicketById(int id);
+        Task<List<Ticket>> GetTicketAll();
+        Task<int> CreateTicket(Ticket Ticket);
+        Task<bool> UpdateTicket(Ticket Ticket);
+        Task<bool> DeleteTicketById(int id);
+    }
+}
