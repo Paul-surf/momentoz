bc5c690bd874aad82f2691a54dce9e4ffde2dcb6
Paul <72189188+Paul-surf@users.noreply.github.com>
2023-11-28 13:19:07 +0100

    Delete WebserverMomentoz directory

--- WebserverMomentoz/WebserverMomentoz/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebserverMomentoz.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
